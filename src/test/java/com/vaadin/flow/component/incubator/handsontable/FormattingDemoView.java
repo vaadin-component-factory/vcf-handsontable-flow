15378cbfa9c17996258c5fc60b538df0ae08ee76
Erik Rehrmann <github@think-outside.eu>
2019-06-28 18:00:58 +0200

    HTML Sample mit reinem Handsontable

--- src/test/java/com/vaadin/flow/component/incubator/handsontable/FormattingDemoView.java
+++ src/test/java/com/vaadin/flow/component/incubator/handsontable/FormattingDemoView.java
@@ -9,34 +9,37 @@ import com.vaadin.flow.router.Route;
 @Route("formatting")
 @StyleSheet("frontend://my-styles.css")
 public class FormattingDemoView extends AbstractDemoView {
-    private static final String SAMPLE_JSON_DATA = "[[1000, 20000, \"05/21/2018\"],[23000, 50000, \"09/13/1978\"]]";
+  private static final String SAMPLE_JSON_DATA =
+      "[[1000, 20000, \"05/21/2018\"],[23000, 50000, \"09/13/1978\"]]";
 
-    public FormattingDemoView() {
-        Handsontable handsontable = new Handsontable(createJsonArray(SAMPLE_JSON_DATA));
-        handsontable.setId("hot1");
+  public FormattingDemoView() {
+    Handsontable handsontable = new Handsontable(createJsonArray(SAMPLE_JSON_DATA));
+    handsontable.setId("hot1");
 
-        Settings settings = new Settings();
-        List<Column> columns = new ArrayList<>();
-        Column column = new Column();
-        column.setType("numeric");
-        column.setNumericFormat(new NumericFormat("0,0.00"));
-        columns.add(column);
+    Settings settings = new Settings();
+    List<Column> columns = new ArrayList<>();
+    Column column = new Column();
+    column.setType("numeric");
+    NumericFormat nf = new NumericFormat("0.0,00");
+    nf.setCulture("de-DE");
+    column.setNumericFormat(nf);
+    columns.add(column);
 
-        column = new Column();
-        column.setType("numeric");
-        column.setNumericFormat(new NumericFormat("0,0"));
-        columns.add(column);
+    column = new Column();
+    column.setType("numeric");
+    column.setNumericFormat(new NumericFormat("0,0"));
+    columns.add(column);
 
-        column = new Column();
-        column.setType("date");
-        column.setDateFormat("MM/DD/YYYY");
-        column.setClassName("yellow-on-blue");
-        columns.add(column);
-        settings.setColumns(columns);
+    column = new Column();
+    column.setType("date");
+    column.setDateFormat("MM/DD/YYYY");
+    column.setClassName("yellow-on-blue");
+    columns.add(column);
+    settings.setColumns(columns);
 
-        handsontable.setSettings(settings);
-        handsontable.setHeaderClassNames(new String[]{"left-header","center-header","right-header"});
+    handsontable.setSettings(settings);
+    handsontable.setHeaderClassNames(new String[] {"left-header", "center-header", "right-header"});
 
-        add(handsontable);
-    }
+    add(handsontable);
+  }
 }
--- src/test/java/com/vaadin/flow/component/incubator/handsontable/FormulaDemoView.java
+++ src/test/java/com/vaadin/flow/component/incubator/handsontable/FormulaDemoView.java
@@ -1,34 +1,58 @@
 package com.vaadin.flow.component.incubator.handsontable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.vaadin.flow.router.Route;
 
 @Route("formula")
 public class FormulaDemoView extends AbstractDemoView {
-    private static final String SAMPLE_JSON_DATA = "[\n" +
-            "    {\n" +
-            "        \"Amount\": 120000,\n" +
-            "        \"TaxRate\": 1.2,\n" +
-            "        \"Payable\": \"=[Amount]+[Amount]*[TaxRate]/100\",\n" +
-            "        \"Tax\": \"=[Payable]-[Amount]\"\n" +
-            "    },\n" +
-            "    {\n" +
-            "        \"Amount\": 50000,\n" +
-            "        \"TaxRate\": 24,\n" +
-            "        \"Payable\": \"=[Amount]+[Amount]*[TaxRate]/100\",\n" +
-            "        \"Tax\": \"=[Payable]-[Amount]\"\n" +
-            "    },\n" +
-            "    {\n" +
-            "        \"Amount\": 300000,\n" +
-            "        \"TaxRate\": 5,\n" +
-            "        \"Payable\": \"=[Amount]+[Amount]*[TaxRate]/100\",\n" +
-            "        \"Tax\": \"=[Payable]-[Amount]\"\n" +
-            "    }\n" +
-            "]";
+  private static final String SAMPLE_JSON_DATA =
+      "[\n"
+          + "    {\n"
+          + "        \"Amount\": 120000,\n"
+          + "        \"TaxRate\": 1.2,\n"
+          + "        \"Payable\": \"=[Amount]+[Amount]*[TaxRate]/100\",\n"
+          + "        \"Tax\": \"=[Payable]-[Amount]\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "        \"Amount\": 50000,\n"
+          + "        \"TaxRate\": 24,\n"
+          + "        \"Payable\": \"=[Amount]+[Amount]*[TaxRate]/100\",\n"
+          + "        \"Tax\": \"=[Payable]-[Amount]\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "        \"Amount\": 300000,\n"
+          + "        \"TaxRate\": 5,\n"
+          + "        \"Payable\": \"=[Amount]+[Amount]*[TaxRate]/100\",\n"
+          + "        \"Tax\": \"=[Payable]-[Amount]\"\n"
+          + "    }\n"
+          + "]";
 
-    public FormulaDemoView() {
-        Handsontable handsontable = new Handsontable(createJsonArray(SAMPLE_JSON_DATA));
-        handsontable.setId("hot1");
+  private static final String[] COL_HEADERS = {"Amount", "TaxRate", "Payable", "Tax"};
 
-        add(handsontable);
-    }
+  public FormulaDemoView() {
+    Handsontable handsontable = new Handsontable(createJsonArray(SAMPLE_JSON_DATA));
+    handsontable.setId("hot1");
+    Settings settings = new Settings();
+    settings.setColHeaders(COL_HEADERS);
+    List<Column> columns = new ArrayList<>();
+    columns.add(getNumericColumn("0.0,00"));
+    columns.add(getNumericColumn("0.0,00"));
+    columns.add(getNumericColumn("0.0,00"));
+    columns.add(getNumericColumn("0.0,00"));
+    settings.setColumns(columns);
+    handsontable.setSettings(settings);
+
+    add(handsontable);
+  }
+
+  private Column getNumericColumn(String format) {
+    NumericFormat nf = new NumericFormat(format);
+    nf.setCulture("de-DE");
+    Column column = new Column();
+    column.setType("numeric");
+    column.setNumericFormat(nf);
+    return column;
+  }
 }
